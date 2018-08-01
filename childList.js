ca468d224ed51b3fecfef3bcf13742d7c4170311
Matthew Phillips <matthew@matthewphillips.info>
2018-08-01 10:47:27 -0400

    Implements removeChild and insertBefore
    This concludes all of the childList methods.

--- childList.js
+++ childList.js
@@ -5,28 +5,34 @@ module.exports = function(Element) {
 	Element.prototype.appendChild = function(node) {
 		var res = appendChild.apply(this, arguments);
 		if(this.ownerDocument) {
-			this.ownerDocument[onChildListSymbol](node.parentNode, node);
+			this.ownerDocument[onChildListSymbol](this, node);
 		}
 		return res;
 	};
 
-	/*
 	var insertBefore = Element.prototype.insertBefore;
-	Element.prototype.insertBefore = function() {
-
+	Element.prototype.insertBefore = function(node) {
+		var res = insertBefore.apply(this, arguments);
+		if(this.ownerDocument) {
+			this.ownerDocument[onChildListSymbol](this, node);
+		}
+		return res;
 	};
 
 	var removeChild = Element.prototype.removeChild;
-	Element.prototype.removeChild = function() {
-
+	Element.prototype.removeChild = function(node) {
+		var res = removeChild.apply(this, arguments);
+		if(this.ownerDocument) {
+			this.ownerDocument[onChildListSymbol](this, null, node);
+		}
+		return res;
 	};
-	*/
 
 	var replaceChild = Element.prototype.replaceChild;
 	Element.prototype.replaceChild = function(newNode, oldNode) {
 		var res = replaceChild.apply(this, arguments);
 		if(this.ownerDocument) {
-			this.ownerDocument[onChildListSymbol](newNode.parentNode, newNode, oldNode);
+			this.ownerDocument[onChildListSymbol](this, newNode, oldNode);
 		}
 		return res;
 	};
